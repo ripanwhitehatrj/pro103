aeff9f7a33edeaf86c4a0ea7befee5b07da814e9
ripanwhitehatrj <88594549+ripanwhitehatrj@users.noreply.github.com>
2022-04-04 16:18:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-.
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'svg',
+    svg_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'">';
+    });
+};
+console.log('ml5 version',ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZS1Pko70B/model.json',modelloaded);
+function modelloaded(){
+    console.log('model is loaded');
+};
